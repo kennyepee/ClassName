2d611f01662f9bbfa966b742fc932998b15f1614
Kenny Epee <kennyepee@Kennys-MacBook-Pro.local>
2020-08-28 18:31:08 -0400

    Initial commit

---
+++ src/com/company/ClassName.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class ClassName {
+
+    public static void main(String[] args){
+        //write your code here
+System.out.println("Kenny");
+    }
+}
